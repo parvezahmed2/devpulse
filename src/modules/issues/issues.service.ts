e381e4472f0fa695e25430b0ba3653ad69d380a1
parvezahmed_24 <parvezahmed9432@gmail.com>
2026-07-06 14:05:29 +0600

    single issue service added

--- src/modules/issues/issues.service.ts
+++ src/modules/issues/issues.service.ts
@@ -212,6 +212,83 @@ return issues;
 
  
 
+const getSingleIssue = async (issueId: number) => {
+  const issueResult = await pool.query(
+      `
+      SELECT
+          id,
+          title,
+          description,
+          type,
+          status,
+          reporter_id,
+          created_at,
+          updated_at
+      FROM issues
+      WHERE id = $1
+      `,
+      [issueId]
+  );
+  if (issueResult.rows.length === 0) {
+      throw new Error("Issue not found");
+  }
+
+
+  const issue = issueResult.rows[0];
+
+
+  const reporterResult = await pool.query(
+      `
+      SELECT
+          id,
+          name,
+          role
+      FROM users
+      WHERE id = $1
+      `,
+      [issue.reporter_id]
+  );
+
+
+  
+
+
+  return {
+
+
+      id: issue.id,
+
+
+      title: issue.title,
+
+
+      description: issue.description,
+
+
+      type: issue.type,
+
+
+      status: issue.status,
+
+
+      reporter: reporterResult.rows[0],
+
+
+      created_at: issue.created_at,
+
+
+      updated_at: issue.updated_at
+
+
+  };
+
+
+};
+
+
+
+
+
  
 
 
@@ -219,6 +296,7 @@ return issues;
 
 export const issueService = {
   createIssue,
-  getAllIssues
+  getAllIssues,
+  getSingleIssue
    
 }
